194793fd64c3a7ab4bf61a0378fc3a89186030bf
Antoine van Wel <antoinevanwel@gmail.com>
2012-01-29 15:23:15 +0100

    improving streaming

--- zipstream.js
+++ zipstream.js
@@ -9,15 +9,18 @@ var util = require('util');
 var crc32 = require('./crc32');
 
 
-//TODO test raw input
-//TODO date
-//TODO improve errors; close down everything properly
-
-
 function ZipStream(opt) {
-  this.current = 0;
-  this.files = [];
-  this.options = opt;
+  var self = this;
+
+  self.readable = true;
+  self.paused = false;
+  self.busy = false;
+  self.eof = false;
+
+  self.queue = [];
+  self.current = 0;
+  self.files = [];
+  self.options = opt;
 }
 
 util.inherits(ZipStream, stream.Stream);
@@ -38,17 +41,114 @@ function convertDate(d) {
 }
 
 
-ZipStream.prototype.deflate = function(source, file, callback) {
+ZipStream.prototype.pause = function() {
+  var self = this;
+  self.paused = true;
+}
+
+ZipStream.prototype.resume = function() {
+  var self = this;
+  self.paused = false;
+
+  self._read();
+}
+
+ZipStream.prototype.destroy = function() {
+  var self = this;
+  self.readable = false;
+}
+
+ZipStream.prototype._read = function() {
   var self = this;
 
-  if (file.deflated) {
-    if (file.crc32 === undefined || file.uncompressed === undefined) {
-      emit('error', 'when supplying deflated data, you must also supply crc32 and uncompressed');
-      return;
-    }
+  if (!self.readable || self.paused) { return; }
+
+  if (self.queue.length > 0) {
+    var data = self.queue.shift();
+    self.emit('data', data);
   }
 
-  // local file header
+  if (self.eof && self.queue.length === 0) {
+    self.emit('end');
+    self.readable = false;
+  }
+
+  process.nextTick(function() { self._read(); }); //TODO improve
+}
+
+
+
+ZipStream.prototype.finalize = function() {
+  var self = this;
+
+  if (self.files.length === 0) { 
+    emit('error', 'no files in zip');
+    return;
+  }
+
+  self._emitCentralDirectory();
+  self.eof = true;
+}
+
+
+ZipStream.prototype.addFile = function(source, file, callback) {
+  var self = this;
+
+  if (self.busy) {
+    emit('error', 'previous file not finished');
+    return;
+  }
+
+  self.busy = true;
+  self.source = source;
+  self.file = file;
+  self.callback = callback;
+  
+  self._emitLocalFileHeader(file);
+
+  var deflate = zlib.createDeflateRaw(self.options);
+  var checksum = crc32.createCRC32();
+  var uncompressed = 0;
+  var compressed = 0;
+  
+  deflate.on('data', function(chunk) { 
+    compressed += chunk.length;
+    self.queue.push(chunk);
+  });
+
+  deflate.on('end', function() {
+    file.crc32 = checksum.digest();
+    file.compressed = compressed;
+    file.uncompressed = uncompressed;
+
+    self.current += compressed;
+    self._emitDataDescriptor(file);
+
+    self.files.push(file);
+    self.busy = false;
+    callback();
+  });
+
+  source.on('data', function(data) {
+    uncompressed += data.length;
+    checksum.update(data);
+    deflate.write(data); //TODO check for false & wait for drain
+  });
+
+  source.on('end', function() { 
+    deflate.end(); 
+  });
+
+  process.nextTick(function() { self._read(); });
+}
+
+//TODO remove listeners on end
+
+
+// local file header
+ZipStream.prototype._emitLocalFileHeader = function(file) {
+  var self = this;
+
   file.version = 20;
   file.bitflag = 8;
   file.method = 8;
@@ -71,148 +171,69 @@ ZipStream.prototype.deflate = function(source, file, callback) {
   buf.writeUInt16LE(0, 28);                 // extra field length
   buf.write(file.name, 30);                 // file name
 
-  self.emit('data', buf);
+  self.queue.push(buf);
   self.current += buf.length;
-
-  if (!file.deflated) {
-  
-    // data
-    var defl = zlib.createDeflateRaw(self.options);
-    var checksum = crc32.createCRC32();
-    var uncompressed = 0;
-    var compressed = 0;
-    
-    defl.on('data', function(chunk) { 
-      compressed += chunk.length;
-      self.emit('data', chunk);
-    });
-
-    defl.on('end', function() {
-      file.crc32 = checksum.digest();
-      file.compressed = compressed;
-      file.uncompressed = uncompressed;
-
-      self.current += compressed;
-
-      // data descriptor
-      var buf = new Buffer(16);
-      buf.writeUInt32LE(0x08074b50, 0);         // data descriptor record signature
-      buf.writeInt32LE(file.crc32, 4);          // crc-32
-      buf.writeUInt32LE(file.compressed, 8);    // compressed size
-      buf.writeUInt32LE(file.uncompressed, 12); // uncompressed size
-
-      self.emit('data', buf);
-      self.current += buf.length;
-      self.files.push(file);
-
-      callback();
-    });
-
-    source.on('data', function(data) {
-      uncompressed += data.length;
-      checksum.update(data);
-      defl.write(data);
-    });
-
-    source.on('end', function() { 
-      defl.end(); 
-    });
-  } else {
-    var compressed = 0;
-
-    // data is deflated
-    source.on('data', function(data) {
-      self.emit('data', data);
-      compressed += data.length;
-    });
-
-    //TODO DRY cleanup. 
-    source.on('end', function() { 
-      file.compressed = compressed;
-
-      self.current += compressed;
-
-      // data descriptor
-      var buf = new Buffer(16);
-      buf.writeUInt32LE(0x08074b50, 0);         // data descriptor record signature
-      buf.writeInt32LE(file.crc32, 4);          // crc-32
-      buf.writeUInt32LE(file.compressed, 8);    // compressed size
-      buf.writeUInt32LE(file.uncompressed, 12); // uncompressed size
-
-      self.emit('data', buf);
-      self.current += buf.length;
-      self.files.push(file);
-
-      callback();
-    });
-  }
-
 }
 
+ZipStream.prototype._emitDataDescriptor = function(file) {
+  var self = this;
 
+  var buf = new Buffer(16);
+  buf.writeUInt32LE(0x08074b50, 0);         // data descriptor record signature
+  buf.writeInt32LE(file.crc32, 4);          // crc-32
+  buf.writeUInt32LE(file.compressed, 8);    // compressed size
+  buf.writeUInt32LE(file.uncompressed, 12); // uncompressed size
 
+  self.queue.push(buf);
+  self.current += buf.length;
+}
 
-
-ZipStream.prototype.finalize = function() {
+ZipStream.prototype._emitCentralDirectory = function() {
   var self = this;
   var cdoffset = self.current;
+
+  var buf = new Buffer(1024);
+  var ptr = 0;
   var cdsize = 0;
 
-  if (self.files.length === 0) { 
-    emit('error', 'no files in zip');
-    return;
-  }
-  
   for (var i=0; i<self.files.length; i++) {
     var file = self.files[i];
 
-    var length = 46 + file.name.length;
-    cdsize += length;
-
     // central directory file header
-    var buf = new Buffer(length);
-    buf.writeUInt32LE(0x02014b50, 0);         // central file header signature 
-    buf.writeUInt16LE(file.version, 4);       // TODO version made by 
-    buf.writeUInt16LE(file.version, 6);       // version needed to extract 
-    buf.writeUInt16LE(file.bitflag, 8);       // general purpose bit flag   
-    buf.writeUInt16LE(file.method, 10);       // compression method
-    buf.writeUInt32LE(file.moddate, 12);      // last mod file time and date
-    buf.writeInt32LE(file.crc32, 16);         // crc-32 
-    buf.writeUInt32LE(file.compressed, 20);   // compressed size  
-    buf.writeUInt32LE(file.uncompressed, 24); // uncompressed size  
-    buf.writeUInt16LE(file.name.length, 28);  // file name length
-    buf.writeUInt16LE(0, 30);                 // extra field length
-    buf.writeUInt16LE(0, 32);                 // file comment length
-    buf.writeUInt16LE(0, 34);                 // disk number where file starts
-    buf.writeUInt16LE(0, 36);                 // internal file attributes
-    buf.writeUInt32LE(0, 38);                 // external file attributes
-    buf.writeUInt32LE(file.offset, 42);       // relative offset
-    buf.write(file.name, 46);                 // file name
+    buf.writeUInt32LE(0x02014b50, ptr+0);         // central file header signature 
+    buf.writeUInt16LE(file.version, ptr+4);       // TODO version made by 
+    buf.writeUInt16LE(file.version, ptr+6);       // version needed to extract 
+    buf.writeUInt16LE(file.bitflag, ptr+8);       // general purpose bit flag   
+    buf.writeUInt16LE(file.method, ptr+10);       // compression method
+    buf.writeUInt32LE(file.moddate, ptr+12);      // last mod file time and date
+    buf.writeInt32LE(file.crc32, ptr+16);         // crc-32 
+    buf.writeUInt32LE(file.compressed, ptr+20);   // compressed size  
+    buf.writeUInt32LE(file.uncompressed, ptr+24); // uncompressed size  
+    buf.writeUInt16LE(file.name.length, ptr+28);  // file name length
+    buf.writeUInt16LE(0, ptr+30);                 // extra field length
+    buf.writeUInt16LE(0, ptr+32);                 // file comment length
+    buf.writeUInt16LE(0, ptr+34);                 // disk number where file starts
+    buf.writeUInt16LE(0, ptr+36);                 // internal file attributes
+    buf.writeUInt32LE(0, ptr+38);                 // external file attributes
+    buf.writeUInt32LE(file.offset, ptr+42);       // relative offset
+    buf.write(file.name, ptr+46);                 // file name
 
-    self.emit('data', buf);
+    ptr = ptr + 46 + file.name.length;
   }
 
+  cdsize = ptr;
   
   // end of central directory record
-  var buf = new Buffer(22);
-  buf.writeUInt32LE(0x06054b50, 0);           // end of central dir signature
-  buf.writeUInt16LE(0, 4);                    // number of this disk
-  buf.writeUInt16LE(0, 6);                    // disk where central directory starts
-  buf.writeUInt16LE(self.files.length, 8);    // number of central directory records on this disk
-  buf.writeUInt16LE(self.files.length, 10);   // total number of central directory records
-  buf.writeUInt32LE(cdsize, 12);              // size of central directory in bytes
-  buf.writeUInt32LE(cdoffset, 16);            // offset of start of central directory, relative to start of archive
-  buf.writeUInt16LE(0, 20);                   // comment length
+  buf.writeUInt32LE(0x06054b50, ptr+0);           // end of central dir signature
+  buf.writeUInt16LE(0, ptr+4);                    // number of this disk
+  buf.writeUInt16LE(0, ptr+6);                    // disk where central directory starts
+  buf.writeUInt16LE(self.files.length, ptr+8);    // number of central directory records on this disk
+  buf.writeUInt16LE(self.files.length, ptr+10);   // total number of central directory records
+  buf.writeUInt32LE(cdsize, ptr+12);              // size of central directory in bytes
+  buf.writeUInt32LE(cdoffset, ptr+16);            // offset of start of central directory, relative to start of archive
+  buf.writeUInt16LE(0, ptr+20);                   // comment length
 
-  self.emit('data', buf);
-  self.emit('end');
+  ptr = ptr + 22;
+
+  self.queue.push(buf.slice(0, ptr));
 }
-
-
-ZipStream.prototype.addFile = function(source, file, callback) {
-  var self = this;
-  
-  process.nextTick(function() { self.deflate(source, file, callback); });
-}
-
-
